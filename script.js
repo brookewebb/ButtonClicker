4d6f75b9293808bd8de28fa2a350c6ddc8372e21
brookewebb <kuonisacoolguy@hotmail.com>
2022-02-01 10:10:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeText(element){
+    element.innerText = "Log Out"
+}
+
+function removeDef(element){
+    element.style.visibility = "hidden"
+}
+
+function like(element){
+    alert("Ninja was liked!")
+}
